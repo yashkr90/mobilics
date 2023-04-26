55861e040f1a015bf13ba599fd211180d5322c17
yashkr90 <kryash90@gmail.com>
2023-04-27 04:49:08 +0530

    change route

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import { useRouter } from 'next/navigation';
+import { usePage } from "@/store/store";
 import Datatable from './components/Datatable';
+
 export default function Home() {
+  
+  useRouter.push(`/tables/${usePage.getState().page}`)
   return (
     <>
     {/* <Page1 /> */}
     this is home page
+    
     {/* <Datatable /> */}
     </>
   )
